796b101345dcd873f75501da5502eee476d6f93d
Ash Laidlaw <ashlaidlaw@gmail.com>
2017-05-10 19:27:44 -0700

    Add router, new routes for different pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PhotographyComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,26 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  },
+  {
+    path: 'photography',
+    component: PhotographyComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
 ];
 
