23840a10dc42a6f7ee977e34973175fbd48464c5
Ash Laidlaw <ashlaidlaw@gmail.com>
2017-05-14 15:44:58 -0700

    Update styling and content

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -6,42 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
-  bobRossHover: boolean = true;
+  bobRossHover: boolean = false;
   hockeyTeamHover: boolean = false;
   shoeStoreHover: boolean = false;
+  projectContent: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  projectContentToggle() {
+    this.projectContent = true;
+    this.bobRossHover = false;
+    this.hockeyTeamHover = false;
+    this.shoeStoreHover = false;
+  }
+
   bobRossToggle() {
-    if (this.bobRossHover) {
-      this.bobRossHover = false;
-    } else {
+    if (!this.bobRossHover) {
       this.bobRossHover = true;
+      this. projectContent = false;
+    } else {
       this.hockeyTeamHover = false;
       this.shoeStoreHover = false;
+      this. projectContent = false;
     }
   }
 
   hockeyTeamToggle() {
-    if (this.hockeyTeamHover) {
-      this.hockeyTeamHover = false;
-    } else {
+    if (!this.hockeyTeamHover) {
       this.hockeyTeamHover = true;
+      this. projectContent = false;
+    } else {
       this.bobRossHover = false;
       this.shoeStoreHover = false;
+      this. projectContent = false;
     }
   }
 
   shoeStoreToggle() {
-    if (this.shoeStoreHover) {
-      this.shoeStoreHover = false;
-    } else {
+    if (!this.shoeStoreHover) {
       this.shoeStoreHover = true;
+      this. projectContent = false;
+    } else {
       this.bobRossHover = false;
       this.hockeyTeamHover = false;
+      this. projectContent = false;
     }
   }
 
