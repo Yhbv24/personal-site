ae78b4b982e28b0ae20d838984a97cb42595b06e
Ash Laidlaw <ashlaidlaw@gmail.com>
2017-05-14 14:31:30 -0700

    Work section overhaul; update styling and layout

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work.component.css']
 })
 export class WorkComponent implements OnInit {
+  bobRossHover: boolean = true;
+  hockeyTeamHover: boolean = false;
+  shoeStoreHover: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  bobRossToggle() {
+    if (this.bobRossHover) {
+      this.bobRossHover = false;
+    } else {
+      this.bobRossHover = true;
+      this.hockeyTeamHover = false;
+      this.shoeStoreHover = false;
+    }
+  }
+
+  hockeyTeamToggle() {
+    if (this.hockeyTeamHover) {
+      this.hockeyTeamHover = false;
+    } else {
+      this.hockeyTeamHover = true;
+      this.bobRossHover = false;
+      this.shoeStoreHover = false;
+    }
+  }
+
+  shoeStoreToggle() {
+    if (this.shoeStoreHover) {
+      this.shoeStoreHover = false;
+    } else {
+      this.shoeStoreHover = true;
+      this.bobRossHover = false;
+      this.hockeyTeamHover = false;
+    }
   }
 
 }
